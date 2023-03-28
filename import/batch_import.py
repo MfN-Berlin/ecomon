f57564d441841126375792e33cb9e468dc46fb7a
BW <benjamin.werner@mfn.berlin>
2023-03-28 19:48:01 +0200

    extend the batch import with folder parameter

--- import/batch_import.py
+++ import/batch_import.py
@@ -1,8 +1,14 @@
 import argparse
 import os
+import glob
 
 
-def main(configs):
+def main(configs, config_folder):
+    # add YAML configs from folder to configs list
+    if config_folder:
+        for file_path in glob.glob(os.path.join(config_folder, "*.yaml")):
+            configs.append(file_path)
+        print("Found configs: ", configs)
     for config in configs:
         os.system(
             "python3 import/import_records.py "
@@ -14,15 +20,18 @@ def main(configs):
 
 if __name__ == "__main__":
     # parse command line arguments and run main
-    # parse a list of config files
+    # parse a list of config files and a path to config folder
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "configs", help="list of config files", type=str, default=None, nargs="*",
     )
+    parser.add_argument(
+        "--config_folder",
+        help="path to folder containing YAML config files",
+        type=str,
+        default=None,
+    )
     args = parser.parse_args()
-    if args.configs:
-        main(args.configs)
-    else:
-        print("No config file specified")
-        exit(1)
-
+    configs = args.configs or []
+    config_folder = args.config_folder
+    main(configs, config_folder)
