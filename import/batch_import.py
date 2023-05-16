a8d7012ea9bc9153e7db2e198fc26e8011c01b9e
root <root@mfn-tsa-5.dmz2.mfn.local>
2023-05-16 13:57:14 +0200

    improve batch import

--- import/batch_import.py
+++ import/batch_import.py
@@ -5,40 +5,40 @@ import glob
 
 def main(
     configs,
-
     retry=False,
     create_report=False,
     drop_index=False,
     create_index=False,
 ):
-    # add YAML configs from folder to configs list
-
-    for config in configs:  #
-        pathes = []
+    # Add YAML configs from folder to configs list
+    for config in configs:
+        paths = []
         if os.path.isdir(config):
+            # Collect all YAML files in the directory
             for file_path in glob.glob(os.path.join(config, "*.yaml")):
-                pathes.append(file_path)
-        else: 
-            pathes.append(config)
-        for path in pathes: 
-            cmd_str = (
-                "python3 import/import_records.py " + path + " --drop_index"
-                if drop_index
-                else "" + " --retry"
-                if retry
-                else "" + " --create_index"
-                if create_index
-                else "" + " --create_report"
-                if (create_report)
-                else ""
-            )
+                paths.append(file_path)
+        else:
+            paths.append(config)
+
+        for path in paths:
+            cmd_str = "python3 import/import_records.py " + path
+
+            if drop_index:
+                cmd_str += " --drop_index"
+            if retry:
+                cmd_str += " --retry"
+            if create_index:
+                cmd_str += " --create_index"
+            if create_report:
+                cmd_str += " --create_report"
+
             print(cmd_str)
             os.system(cmd_str)
 
 
 if __name__ == "__main__":
-    # parse command line arguments and run main
-    # parse a list of config files and a path to config folder
+    # Parse command line arguments and run main
+    # Parse a list of config files and a path to the config folder
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "configs", help="list of config files", type=str, default=None, nargs="*",
@@ -55,14 +55,13 @@ if __name__ == "__main__":
         "--retry", help="retry ", action="store_true", default=False,
     )
     parser.add_argument(
-        "--create_report", help="create json report", action="store_true", default=False
+        "--create_report", help="create JSON report", action="store_true", default=False
     )
     args = parser.parse_args()
     configs = args.configs or []
 
     main(
         configs,
-        
         retry=args.retry,
         create_index=args.create_index,
         drop_index=args.drop_index,
