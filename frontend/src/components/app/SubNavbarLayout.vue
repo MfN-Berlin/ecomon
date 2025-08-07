a4e3bc0803a139d5f5288a02eb453dcc0d0a1952
Alvaro Ortiz <alvaro@smultron.org>
2025-08-07 14:05:34 +0200

    Force main bar and collapsible sidebar on all pages

--- frontend/src/components/app/SubNavbarLayout.vue
+++ frontend/src/components/app/SubNavbarLayout.vue
@@ -1,4 +1,15 @@
 <template>
+  <app-navigation-main-bar></app-navigation-main-bar>
+
+  <app-navigation-side-drawer> </app-navigation-side-drawer>
+  <v-navigation-drawer width="350" mobile-breakpoint="sm">
+    <slot name="sub-nav-bar"></slot>
+  </v-navigation-drawer>
+  <slot name="content"></slot>
+</template>
+
+<!--
+<template>
   <transition name="fade">
     <app-navigation-main-bar v-if="!$vuetify.display.lgAndUp"></app-navigation-main-bar>
     <app-bread-crumbs
@@ -15,3 +26,4 @@
   </v-navigation-drawer>
   <slot name="content"></slot>
 </template>
+-->
