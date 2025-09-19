395c6595f52e174644e4efaf5147151af59bfe95
Alvaro Ortiz <alvaro@smultron.org>
2025-09-19 11:10:10 +0200

    temporary fix for dashboard routing

--- frontend/src/pages/dashboard/index.vue
+++ frontend/src/pages/dashboard/index.vue
@@ -207,7 +207,7 @@ const dashboardAppUrl = computed(() => {
   // Make sure selectedParams exists before accessing its properties
   if (!selectedParams || !selectedParams.value) {
     console.error("selectedParams is undefined");
-    return "/ecomon_next/dashboard"; // Return base URL without parameters
+    return "https://129.70.51.241/ecomon_next/dashboard/"; // Return base URL without parameters
   }
 
   const params = new URLSearchParams();
@@ -270,11 +270,11 @@ const dashboardAppUrl = computed(() => {
     }
   } catch (e) {
     console.error("Error generating dashboard URL:", e);
-    return "/ecomon_next/dashboard"; // Return base URL in case of errors
+    return "https://129.70.51.241/ecomon_next/dashboard/"; // Return base URL in case of errors
   }
 
   // Return the base URL with query parameters
-  return `/ecomon_next/dashboard?${params.toString()}`;
+  return `https://129.70.51.241/ecomon_next/dashboard?${params.toString()}`;
 });
 
 watch(
