4af8125bdda05393a65dd51551a71ec16883d33f
Alvaro Ortiz <alvaro@smultron.org>
2026-07-10 16:05:20 +0200

    Hasura definitions for thresholds table

---
+++ scripts/count_wavs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+find . -maxdepth 1 -type d -name 'AKWAMO*' | sort | while read -r dir; do
+    dirname=$(basename "$dir")
+    date_part="${dirname#*_}"
+    if [ "$date_part" -ge "202511" ]; then
+        wav_count=$(find "$dir" -type f -name "*.wav" | wc -l)
+        echo "$dir: $wav_count"
+    fi
+done
